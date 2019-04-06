770e573f783861cdedcb1f21759000e580e998a1
anson <all197@ucsd.edu>
2019-04-06 16:01:16 -0700

    IO steering, gas pedal, brake pedal

--- Assets/testIO.cs
+++ Assets/testIO.cs
@@ -10,10 +10,157 @@ public class testIO : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if ( Input.GetButtonDown("KeyCode.Space"))
+	void Update() {
+
+
+
+
+
+		if ( Input.GetKey(KeyCode.Space))
         {
             Debug.Log("SPACEBAR DETECTED");
         }
-	}
+
+        
+
+        if ( Input.GetAxis("Horizontal") != 0)
+        {
+            Debug.Log("STEERING");
+            Debug.Log(Input.GetAxis("Horizontal"));
+        }
+
+        if ( Input.GetAxis("Vertical") != -1 && Input.GetAxis("Vertical") != 0 )
+        {
+            Debug.Log("GAAAAAAAAAAAAS");
+            Debug.Log(Input.GetAxis("Vertical"));
+        }
+
+        if ( Input.GetAxis("Mouse ScrollWheel") != 0 && Input.GetAxis("Mouse ScrollWheel") != 0.1f )
+        {
+            Debug.Log("Breaks");
+            Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if (Input.GetKey(KeyCode.JoystickButton0))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if ( Input.GetKey(KeyCode.Joystick1Button1))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if (Input.GetKey(KeyCode.Joystick1Button2))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if (Input.GetKey(KeyCode.Joystick1Button3))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button4))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button5))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button6))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button7))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if (Input.GetKey(KeyCode.Joystick1Button8))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button9))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button10))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button11))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button12))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button13))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button14))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if (Input.GetKey(KeyCode.Joystick1Button15))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button16))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button17))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+        if (Input.GetKey(KeyCode.Joystick1Button18))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button19))
+        {
+            Debug.Log("JOYSTICK DETECTED");
+        }
+
+
+
+
+
+
+
+
+
+    }
 }
